7185a6c6f541868b93948d8214a90b359b527fef
Sergey Mochalov <proweb@users.noreply.github.com>
2023-08-22 00:05:01 +0300

    add transform for html minify

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,38 @@
-module.exports = function(eleventyConfig) {
-	return {
-		dir: {
-			input: "src"
-		}
-	}
+
+const { minify } = require('html-minifier-terser');
+
+module.exports = function (eleventyConfig) {
+
+
+	// Transform example
+	// @url: https://www.11ty.dev/docs/config/#transforms
+  eleventyConfig.addTransform("logger", async function (content) {
+   
+    // Eleventy 2.0+ has full access to Eleventy’s `page` variable
+    console.log(`source file: ${this.page.inputPath}`);
+    console.log(`output file: ${this.page.outputPath}`);
+
+    return content; // no change done.
+  });
+
+  // Transforms : Minify HTML Output
+  eleventyConfig.addTransform("htmlmin", function(content) {
+    // Prior to Eleventy 2.0: use this.outputPath instead
+    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+      let minified = minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+      return minified;
+    }
+
+	return content;
+	});
+
+  return {
+    dir: {
+      input: "src",
+    },
+  };
 };

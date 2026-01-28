75caa0bfb0b2b25946551df5ec4373a10ef607a7
Sergey Mochalov <proweb@users.noreply.github.com>
2026-01-29 00:18:41 +0300

    update config

--- .eleventy.js
+++ .eleventy.js
@@ -1,37 +1,36 @@
-
-const { minify } = require('html-minifier-terser');
+const { minify } = require("html-minifier-terser");
 
 /**
  *  @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  */
 
 module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addPassthroughCopy({"public": "/"});
+  eleventyConfig.addPassthroughCopy({ public: "/" });
 
   // Shortcodes
   eleventyConfig.addShortcode("year", () => new Date().getFullYear());
 
   // Transforms : Minify HTML Output
-  eleventyConfig.addTransform("htmlmin", function(content) {
+  eleventyConfig.addTransform("htmlmin", function (content) {
     // Prior to Eleventy 2.0: use this.outputPath instead
-    if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
       let minified = minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
       return minified;
     }
 
-	return content;
-	});
+    return content;
+  });
 
   return {
     dir: {
       input: "src",
-      output: "dist"
+      includes: "_includes",
+      output: "dist",
     },
-    prefix: "/"
+    prefix: "/",
   };
 };
